45bb8f226468ee40bd23cbb938c861abe1c179ee
Jiaqi Duan <jiaqiduan@Jiaqis-MacBook-Pro.local>
2018-02-14 23:59:41 -0800

    add same name check function

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require('express');
-const hlsFetcher = require('hls-fetcher');
+/*
+Modified some parts of the hls-fetcher npm package
+like check same name file
+and return the file name after download.
+ */
+const hlsFetcher = require('./util/index');
 const mkdirp = require('mkdirp');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-
+//enable CORS through express middleware
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next();
 });
+
 
 app.use(express.static('downloads'))
 
@@ -31,18 +37,17 @@ app.get('/stream',(req,res)=>{
             uri: remoteHLS,
             cwd: output
         };
-        hlsFetcher.getIt(options,(err)=>{
+        hlsFetcher.getIt(options,(err,fileName)=>{
             if(err){
+                console.log('here');
                 return res.status(400).send(err);
             }
             else{
-
-                res.send('ok');
+                var fullUrl = `${req.protocol}://${req.get('host')}/${fileName}`;
+                res.send(fullUrl);
             }
         })
     })
-
-
 })
 
 
