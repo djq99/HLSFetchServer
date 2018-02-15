249beb13e8657b8f0fba35dda552f4aedf26246c
Jiaqi Duan <jiaqiduan@Jiaqis-MacBook-Pro.local>
2018-02-14 20:51:45 -0800

    correct spell error

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const hlsFetcher = require('hls-fetcher');
 const mkdirp = require('mkdirp');
 
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/',(req,res) =>{
     res.send('Hello World');
